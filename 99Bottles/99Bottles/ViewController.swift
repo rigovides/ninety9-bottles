1d6fc2d69a5f6d60a1427edf466dfde9dc964bbc
Rigo Vides <rigoberto vides@tdg-mbl-gdl448.local>
2019-03-08 09:42:35 -0600

    added project folder

---
+++ 99Bottles/99Bottles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  99Bottles
+//
+//  Created by Rigoberto Antonio Vides Rodriguez on 3/7/19.
+//  Copyright © 2019 Rigoberto Antonio Vides Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 99Bottles/99BottlesTests/BottlesTests.swift
@@ -0,0 +1,47 @@
+//
+//  BottlesTests.swift
+//  99BottlesTests
+//
+//  Created by Rigoberto Antonio Vides Rodriguez on 3/7/19.
+//  Copyright © 2019 Rigoberto Antonio Vides Rodriguez. All rights reserved.
+//
+
+import XCTest
+
+class BottlesTests: XCTestCase {
+    //    input 99
+    //    test result is:
+    //    "99 bottles of beer on the wall, 99 bottles of beer.
+    //    Take one down and pass it around, 98 bottles of beer on the wall."
+    func testFirstVerse() {}
+
+    //    input 42
+    //    test result is:
+    //    "42 bottles of beer on the wall, 42 bottles of beer.
+    //    Take one down and pass it around, 41 bottles of beer on the wall."
+    func testVerse42() {}
+
+
+    //    input 42
+    //    test result is:
+    //    "42 bottles of beer on the wall, 42 bottles of beer.
+    //    Take one down and pass it around, 41 bottles of beer on the wall."
+    func testTwoVerses() {}
+
+    //    inputs 66, 64
+    //    test result is:
+    //    "66 bottles of beer on the wall, 66 bottles of beer.
+    //    Take one down and pass it around, 65 bottles of beer on the wall.
+    //
+    //    65 bottles of beer on the wall, 65 bottles of beer.
+    //    Take one down and pass it around, 64 bottles of beer on the wall.
+    //
+    //    64 bottles of beer on the wall, 64 bottles of beer.
+    //    Take one down and pass it around, 63 bottles of beer on the wall."
+    func testVerse0() {}
+
+    //    test result is equal to bottlesSong
+    func testSong() {
+        bottlesSong
+    }
+}
